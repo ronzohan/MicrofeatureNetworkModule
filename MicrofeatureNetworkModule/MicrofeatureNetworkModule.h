0e1c5a809760d092ea1e0632d52f93b987fa627f
Ron Daryl Magno <ron.zohan@gmail.com>
2017-12-11 01:10:35 +0800

    Initla commit

---
+++ MicrofeatureNetworkModule/MicrofeatureNetworkModule.h
@@ -0,0 +1,19 @@
+//
+//  MicrofeatureNetworkModule.h
+//  MicrofeatureNetworkModule
+//
+//  Created by Ron Daryl Magno on 12/11/17.
+//  Copyright © 2017 Ron Daryl Magno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MicrofeatureNetworkModule.
+FOUNDATION_EXPORT double MicrofeatureNetworkModuleVersionNumber;
+
+//! Project version string for MicrofeatureNetworkModule.
+FOUNDATION_EXPORT const unsigned char MicrofeatureNetworkModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MicrofeatureNetworkModule/PublicHeader.h>
+
+
